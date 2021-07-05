60c1c847e8e48db2d3ef4d17d1d26339b5df3d0a
mahipalsinh <nakummahipalsinh@gmail.com>
2021-07-05 21:29:47 +0530

    lesson 2 complete

---
+++ lib/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final int years = 19;
+    final String name = "Mahipalsinh";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("I am $name and i am $years old."),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
