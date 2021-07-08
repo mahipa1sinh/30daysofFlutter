f86e52a00b7eab302cc8a6b86d4739cd33219d0d
mahipalsinh <nakummahipalsinh@gmail.com>
2021-07-08 11:22:03 +0530

    day12 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -6,5 +6,22 @@ class Item {
   final String color;
   final String image;
 
-  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+  Item(
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
 }
+
+final products = [
+  Item(
+      id: "Mahipalsinh101",
+      name: "iphone 12 Pro",
+      price: 999,
+      color: "#33505a",
+      image:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqG3tGowZTiforo7Cptg_DLMNz0xCxLGPTTC0QQ5Ah-QvHyqWoZfGJTckuLs8e5J0vfkp0nco&usqp=CAc',
+      desc: '')
+];
