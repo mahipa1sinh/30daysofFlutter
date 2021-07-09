19fcbbdad3b5921e71436d70c211bbc68fe9b684
mahipalsinh <nakummahipalsinh@gmail.com>
2021-07-09 12:10:08 +0530

    day17 completed

--- lib/widgets/item_widget.dart
+++ lib/widgets/item_widget.dart
@@ -5,7 +5,6 @@ class ItemWidget extends StatelessWidget {
   final Item item;
 
   const ItemWidget({Key? key, required this.item})
-      // TODO: check it again
       // ignore: unnecessary_null_comparison
       : assert(item != null),
         super(key: key);
